6033d06d8ab0ff3c47b3839d8d083d58f6358593
Rezaur Rahman Ratul <88153639+Rezaur16@users.noreply.github.com>
2023-06-01 10:45:10 +0600

    Create Timus1000.cpp

---
+++ Timus1000.cpp
@@ -0,0 +1,21 @@
+//Timus 1000 Solution
+//#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ///C++ language
+    int a,b;
+    cin>>a>>b;
+   // cin>>b;
+    cout<<a+b<<endl;
+
+
+   /// C language
+   /* int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",a+b);
+    return 0 ;
+    */
+}
